75dec2d327749454f5b87e3699b1cd93ef3e944e
robzilla1738 <135086771+robzilla1738@users.noreply.github.com>
2024-03-22 12:40:23 -0500

    Create models.py

---
+++ models.py
@@ -0,0 +1,8 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    phone_number = db.Column(db.String(20), unique=True, nullable=False)
+    # Add more fields as needed
